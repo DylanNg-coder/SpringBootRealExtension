68db4a04409dcb503da9dc106be12931d66e6dea
DylanNg-coder <p3rs0nal1tY>
2021-02-12 10:05:21 +0000

    first commit

---
+++ src/main/java/com/omanda/SpringBootExtension/SpringBootExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.omanda.SpringBootExtension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExtensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExtensionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omanda/SpringBootExtension/SpringBootExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omanda.SpringBootExtension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExtensionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
